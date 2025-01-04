6487469dcc6a5398ae396072a1a02737c2f69963
Quinten-14 <raymaekersq@gmail.com>
2025-01-04 01:31:49 +0100

    Trigger workflow test

--- src/main/java/Zappy/Main.java
+++ src/main/java/Zappy/Main.java
@@ -1,5 +1,6 @@
 package Zappy;
 
+// testing out a comment to force the workflow to trigger the github action
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
