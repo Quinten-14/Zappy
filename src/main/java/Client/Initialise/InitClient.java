2589ef184a181ff2ea4fbfa6c9cdb15f43bf2bd7
ahooghe <123111553+ahooghe@users.noreply.github.com>
2025-01-05 00:33:27 +0100

    Added initialisation handler

--- src/main/java/Client/Initialise/InitClient.java
+++ src/main/java/Client/Initialise/InitClient.java
@@ -1,8 +1,48 @@
 package Client.Initialise;
 
-public class InitClient {
-    public void initClient()
+import static java.lang.System.exit;
+
+public class InitClient
+{
+    //Storage variables for the team name, the port integer and the host ip
+    String team = null, portStr = null, host = "localhost";
+    int port = 0;
+
+    //Converts input data to local data
+    private void handleData(String[] args)
     {
-        System.out.println("Initialising Client");
+        for (int i = 0; i < args.length; i++)
+        {
+            switch (args[i]) {
+                case "-n" -> team = args[++i];
+                case "-p" -> portStr = args[++i];
+                case "-h" -> host = args[++i];
+                default -> exit(1);
+            }
+        }
+        if (portStr == null || team == null)
+            exit(1);
+        try
+        {
+            port = Integer.parseInt(portStr);
+            portStr = null;
+        }
+        catch (NumberFormatException e)
+        {
+            exit(1);
+        }
+    }
+
+    private void connect()
+    {
+        //try to connect to server using host:port and send the string 'team' as the first communication.
+        // If the server responds with 1 integer, it wants us to make that amount of threads and connect them one by one.
+        // If it responds with 2 integers, continue to game loop
+    }
+
+    public void initClient(String[] args)
+    {
+        handleData(args);
+        connect();
     }
 }
--- src/main/java/Client/Main.java
+++ src/main/java/Client/Main.java
@@ -1,10 +1,20 @@
 package Client;
 
+import Client.Game.Loop;
 import Client.Initialise.InitClient;
 
 public class Main {
     public static void main(String[] args) {
+        if (args.length == 0)
+        {
+            System.out.println("Usage: java Client -n <teamname> -p <port> [-h <hostname>]");
+            System.out.println("-n team name");
+            System.out.println("-p port");
+            System.out.println("-h name of the host, by default it'll be localhost");
+            return;
+        }
+
         InitClient client = new InitClient();
-        client.initClient();
+        client.initClient(args);
     }
 }
