1f710c934f197eb4c8cef13328324a1de5a576de
Quinten Raymaekers <67979828+Quinten-14@users.noreply.github.com>
2025-01-04 02:02:32 +0100

    Update Main.java
    Testing out the required pipeline pass

--- src/main/java/Zappy/Main.java
+++ src/main/java/Zappy/Main.java
@@ -1,6 +1,5 @@
 package Zappy;
 
-// testing out a comment to force the workflow to trigger the github action
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
